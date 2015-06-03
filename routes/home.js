111f67123d036badd22bac006861842e39a66058
Yolo <ou.ye.canada@hotmail.com>
2015-06-03 10:56:05 -0400

    Plannin to display music list in home page

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	res.render('home', {notif: req.flash('notif'),
-					auth: req.session.authenticated});		
+
+	// Check with data in MySQL
+	req.getConnection(function (err, conn) {
+		if (err){
+			console.log(err);
+			return ;
+		}
+		
+		var sql = 'SELECT * FROM music;';
+					
+		conn.query(sql, function(err, rows) {
+			if (err) {
+				console.log(err);
+			}
+			
+			res.render('home', {notif: req.flash('notif'),
+				 auth: req.session.authenticated,
+				 data: rows});
+		});
+	});
 });
 
 module.exports = router;
